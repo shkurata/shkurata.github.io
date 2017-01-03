ce95b97e8ec0df28bbb8910223b39edf2b3df152
Nikolay Neykov <shkurata@MacBookAir.local>
2017-01-03 16:48:03 +0100

    navigation menu improvement

--- js/script.js
+++ js/script.js
@@ -7,5 +7,20 @@ $(document).ready(function() {
     }, 500);
   });
 
-  
+  $(window).on('scroll', function() {
+    var currentPossition = $(this).scrollTop();
+
+    $('section').each(function() {
+      var top = $(this).offset().top - $('nav').outerHeight(),
+      bottom = top + $(this).outerHeight();
+
+      if (currentPossition >= top && currentPossition <= bottom) {
+        $('nav').find('a').removeClass('activeNavItem');
+        $('nav').find('a[href="#' + $(this).attr('id') + '"]')
+          .toggleClass('activeNavItem');
+      }
+
+    });
+  });
+
 });
